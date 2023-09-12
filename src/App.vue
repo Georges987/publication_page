508dc40cb2752cf9a41e47f77033d9d815fb01fa
= <=>
2023-09-12 21:45:21 +0100

    Adding nav and footer

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <v-app>
-    <v-main>
-      <router-view/>
+    <v-app-bar color="transparent" style="backdrop-filter: blur(100px)" fixed>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-title>
+        Georges AYENI
+      </v-app-bar-title>
+    </v-app-bar>
+    <v-navigation-drawer scrim style="backdrop-filter: blur(100px)" color="transparent" v-model="drawer"></v-navigation-drawer>
+    <v-main class="mt-n5">
+      <router-view />
     </v-main>
+    <v-footer color="transparent" style="backdrop-filter: blur(100px)" border elevation="10" height="100">
+      <div>@Georges AYENI</div>
+      <v-spacer></v-spacer>
+      <v-btn class="mx-2" flat @click="link('https://github.com/Georges987')" icon="mdi-github" color="white"></v-btn>
+      <v-btn class="mx-2" flat @click="link('https://facebook.com/georges.ayeni.7')" icon="mdi-facebook"
+        color="white"></v-btn>
+      <v-btn class="mx-2" flat @click="link('https://www.linkedin.com/in/georges-ayeni-542a461bb/')" icon="mdi-linkedin"
+        color="white"></v-btn>
+    </v-footer>
   </v-app>
 </template>
 
@@ -12,7 +28,13 @@ export default {
   name: 'App',
 
   data: () => ({
-    //
+    drawer: false,
   }),
+
+  methods: {
+    link(url) {
+      window.open(url, '_blank');
+    }
+  },
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,40 @@
 <template>
-      <v-carousel height="100vh" cycle :progress="true">
-        <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover reverse-transition="fade-transition"
-          transition="fade-transition">
-          <v-sheet color="transparent" class="fill-height ma-0">
-            <v-row class="fill-height" align="center" justify="center">
-              <div class="display-3">{{ item.text }}</div>
-            </v-row>
-          </v-sheet>
-        </v-carousel-item>
-      </v-carousel>
+  <v-carousel height="100vh" cycle :progress="true">
+    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover reverse-transition="fade-transition"
+      transition="fade-transition">
+      <v-sheet color="transparent" class="fill-height ma-0">
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="text-h3 text-white font-weight-bold">{{ item.text }}</div>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 
-      <v-container class="my-16">
-        <v-card>
-          <v-img src="/images/sqflite.png"></v-img>
-          <v-card-title>
-            Exemple test with sqflite and flutter framework
-          </v-card-title>
-          <v-card-actions>
-            <v-btn append-icon="mdi-github" @click="download('https://github.com/Georges987/flutter_sqflite_student')">
-              Accéder au repositorie Git
-            </v-btn>
-            <v-btn append-icon="mdi-download" @click="download('/files/sqflite-student.apk')">
-              Télécharger l'application
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-container>
+  <v-container class="text-center my-10">
+    Bienvenue sur ma page de publication.
+    Ici, vous trouverez la plupart de mes créations et pourrai y accéder gratuitement.
+    N'hésitez pas à m'envoyer un mail où à tout simplement me contacter dès que vous
+    ressentirez la nécessité. Encore Bienvenue.
+  </v-container>
+  <v-divider inset color="primary" length="500"></v-divider>
+
+  <v-container class="my-16">
+    <v-card elevation="15">
+      <v-img aspect-ratio="1/2" cover height="500" src="/images/sqflite.png"></v-img>
+      <v-card-title>
+        Exemple test with sqflite and flutter framework
+      </v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn prepend-icon="mdi-github" @click="download('https://github.com/Georges987/flutter_sqflite_student')">
+          Accéder au repositorie Git
+        </v-btn>
+        <v-btn prepend-icon="mdi-download" @click="download('/files/sqflite-student.apk')">
+          Télécharger l'application
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
@@ -34,11 +43,11 @@ export default
   {
     name: 'HomeView',
     methods:
-      {
-        download(url) {
-          window.open(url, '_blank');
-        },
+    {
+      download(url) {
+        window.open(url, '_blank');
       },
+    },
 
     data: () => ({
       publications: [
